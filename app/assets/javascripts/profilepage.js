cd7f04caca6a1eeacee55719015c2ea7e2ac340b
Aaron Figueroa <aaronfigueroa@Aarons-iMac.local>
2016-05-23 17:00:21 -0400

    added js viewing friends feature

--- app/assets/javascripts/profilepage.js
+++ app/assets/javascripts/profilepage.js
@@ -1,18 +1,35 @@
 $('document').ready(function(){
 
   $('#friends').click(function(){
-    debugger; 
-    alert($('#test').attr('id'));
-
-    viewFriends(this);
+    viewFriends();
   });
 
 })
 
-function viewFriends(info){
+var url = this.window.location.pathname;
+
+function viewFriends(){
 
 
+  $.getJSON(url).done(function(response) {
+    showFriends(response.user.friends);
+  })
+
+}
+
+function showFriends(friends){
+  var dom = $();
+
+debugger;
+  friends.forEach(function(friend){
+    debugger;
+    dom = dom.add(showFriend(friend));
+  })
+    $("#allFriends").html(dom);
+}
+
+var showFriend = function(friend) {
   debugger;
-
-
+  // return $('<li>', { 'data-friendid': friend.id, text: friend.name});
+    return "<ul><a href=" + friend.id + ">" + friend.name + "</a></ul>";
 }
